13b467e2e22a97c955c49980ce67d34ced898ebd
Deketelaere <bryan.deketelaere@student.howest.be>
2026-06-05 17:53:52 +0200

    #39 last part of the camera kit tutorial thingies

---
+++ tests/lensstudio/CameraKitWebReactTypeScript/StartingProject/src/contexts/CameraKitContext.tsx
@@ -0,0 +1,50 @@
+import { bootstrapCameraKit, CameraKitSession, Lens } from '@snap/camera-kit';
+import { createContext, useEffect, useRef, useState } from 'react';
+
+const apiToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzgwNjcxNjY0LCJzdWIiOiI5ZWY0MGRhNC00MTE2LTRhYzQtYTA5ZC1hN2ZhM2YzMmY3ODl-U1RBR0lOR343MWMzYTBmNC02NTcxLTRlZmItOGM1OS02MmZiOGMwYzhhNWYifQ.Jk60HZ2FzcuF4OlxJwlPs7ptviVNHEQ2YBY1H-B3j7k';
+const lensGroupId = 'a611e74f-a161-4160-b1b1-5c5bcf17f2ff';
+
+
+export interface CameraKitState {
+    session: CameraKitSession;
+    lenses: Lens[];
+}
+
+export const CameraKitContext = createContext<CameraKitState | null>(null);
+
+export const CameraKit: React.FC<{ children: React.ReactNode }> = ({
+    children,
+}) => {
+
+    const isInitialized = useRef<boolean>(false);
+    const [session, setSession] = useState<CameraKitSession | null>(null);
+    const [lenses, setLenses] = useState<Lens[]>([]);
+
+    useEffect(() => {
+        const initializeCameraKit = async () => {
+            const cameraKit = await bootstrapCameraKit({ apiToken });
+            const session = await cameraKit.createSession();
+            const { lenses } = await cameraKit.lensRepository.loadLensGroups([
+                lensGroupId,
+            ]);
+
+            setLenses(lenses);
+            setSession(session);
+        };
+
+        if (isInitialized.current) return;
+        isInitialized.current = true;
+
+        initializeCameraKit();
+    }, []);
+
+
+    return !session ? (
+        <div>Initializing Camera Kit...</div>
+    ) : (
+        <CameraKitContext.Provider value={{ session, lenses }}>
+            {children}
+        </CameraKitContext.Provider>
+    );
+};
+
