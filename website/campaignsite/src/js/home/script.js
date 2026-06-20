048c9d9e8422e1738cdb78b74955d72d2534ed5c
Deketelaere <bryan.deketelaere@student.howest.be>
2026-06-20 04:45:00 +0200

    #58 made some stuff more efficient and also changed dynamic navigation, active class when on that specific page

--- website/campaignsite/src/js/home/script.js
+++ website/campaignsite/src/js/home/script.js
@@ -3,9 +3,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const routes = document.querySelectorAll(".pov__name");
-console.log(routes);
-
 const handleToggleHero = () => {
     let $heroImg = document.querySelectorAll('.hero__image');
     let $carouselDot = document.querySelectorAll('.carousel__dot');
@@ -33,29 +30,8 @@ const handleToggleHero = () => {
     }, 2500);
 }
 
-const handleToggleRoute = (e) => {
-    const $currentRoute = document.querySelector(".pov__route--active");
-    const $currentClicked = document.querySelector(".pov__name--active");
-    console.log($currentRoute);
-
-    const clickedTarget = e.target;
-
-    const clickedTargetRoute = document.querySelector(`#${clickedTarget.dataset.pov}`);
-
-    $currentClicked.classList.toggle("pov__name--active");
-    clickedTarget.classList.toggle("pov__name--active");
-
-    $currentRoute.classList.toggle("pov__route--active")
-    clickedTargetRoute.classList.toggle("pov__route--active");
-    
-}
-
 const init = () => {
-    console.log("hey");
-    // handleToggleHero();
-    routes.forEach(route => {
-        route.addEventListener("click", handleToggleRoute);
-    })
+    handleToggleHero();
 }
 
 init()
